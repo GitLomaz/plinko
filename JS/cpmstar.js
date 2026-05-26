e3fc8a90ec92b3f5ac3205c6326390e5eaf191e2
Ian Lomas <you@example.com>
2026-05-26 13:36:41 -0400

    Make ad system optional - game loads with adblockers

--- JS/cpmstar.js
+++ JS/cpmstar.js
@@ -1,18 +1,28 @@
 (function (zonefile) {
-  var rnd = Math.round(Math.random() * 999999);
-  var s = document.createElement("script");
-  s.type = "text/javascript";
-  s.async = true;
-  var proto = document.location.protocol;
-  var host = proto == "https:" || proto == "file:" ? "https://server" : "//cdn";
-  if (window.location.hash == "#cpmstarDev") host = "//dev.server";
-  if (window.location.hash == "#cpmstarStaging") host = "//staging.server";
-  s.src = host + ".cpmstar.com/cached/zonefiles/" + zonefile + ".js?rnd=" + rnd;
-  var s2 = document.getElementsByTagName("script")[0];
-  s2.parentNode.insertBefore(s, s2);
-  var y = "cpmstarx";
-  var drutObj = (window[y] = window[y] || {});
-  window.cpmstarAPI = function (o) {
-    (drutObj.cmd = drutObj.cmd || []).push(o);
-  };
+  try {
+    var rnd = Math.round(Math.random() * 999999);
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    var proto = document.location.protocol;
+    var host = proto == "https:" || proto == "file:" ? "https://server" : "//cdn";
+    if (window.location.hash == "#cpmstarDev") host = "//dev.server";
+    if (window.location.hash == "#cpmstarStaging") host = "//staging.server";
+    s.src = host + ".cpmstar.com/cached/zonefiles/" + zonefile + ".js?rnd=" + rnd;
+    
+    // Handle script loading errors (e.g., adblockers)
+    s.onerror = function() {
+      console.log('Ad script blocked or failed to load - continuing without ads');
+    };
+    
+    var s2 = document.getElementsByTagName("script")[0];
+    s2.parentNode.insertBefore(s, s2);
+    var y = "cpmstarx";
+    var drutObj = (window[y] = window[y] || {});
+    window.cpmstarAPI = function (o) {
+      (drutObj.cmd = drutObj.cmd || []).push(o);
+    };
+  } catch (e) {
+    console.log('Ad system initialization failed - continuing without ads');
+  }
 })("205_25736_gameapi");
